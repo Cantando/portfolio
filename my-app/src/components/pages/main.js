1802740e4d60d8fe2f2b88acc96ef01306f32e24
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-13 02:30:05 -0400

    added my pages

--- my-app/src/components/pages/main.js
+++ my-app/src/components/pages/main.js
@@ -8,6 +8,16 @@ import Projects from './projects';
 import Resume from './resume';
 import Technology from './technology';
 
+const Main = () => {
+    <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/aboutme" component={AboutMe} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/technology" component={Technology} />
+    </Switch>
+}
 
 
 export default Main;
