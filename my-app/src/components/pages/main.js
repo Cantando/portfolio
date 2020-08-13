b3f3247e0ee69a5a1c4e84dcafc1a3bc250c1fb6
Cantando <65988365+Cantando@users.noreply.github.com>
2020-08-13 02:29:01 -0400

    added a component folder in my src

---
+++ my-app/src/components/pages/main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './home';
+import AboutMe from './aboutme';
+import Contact from './contact';
+import Projects from './projects';
+import Resume from './resume';
+import Technology from './technology';
+
+
+
+export default Main;
